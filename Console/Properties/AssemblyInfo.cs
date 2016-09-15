0b4045b6007d42c7db734157fc75bd09e5d23d22
zeb <zeb@cyclerouter.co.uk>
2016-09-15 14:12:37 +0100

    Ensure version numbers are correct

--- Console/Properties/AssemblyInfo.cs
+++ Console/Properties/AssemblyInfo.cs
@@ -1,16 +1,42 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿//------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Zebedee Mason">
+//     Copyright (c) 2016 Zebedee Mason.
+//
+//      The author's copyright is expressed through the following notice, thus
+//      giving effective rights to copy and use this software to anyone, as shown
+//      in the license text.
+//
+//     NOTICE:
+//      This software is released under the terms of the GNU Lesser General Public
+//      license; a copy of the text has been released with this package (see file
+//      LICENSE, where the license text also appears), and can be found on the
+//      GNU web site, at the following address:
+//
+//           http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+//
+//      Please refer to the license text for any license information. This notice
+//      has to be considered part of the license, and should be kept on every copy
+//      integral or modified, of the source files. The removal of the reference to
+//      the license will be considered an infringement of the license itself.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Console")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Console")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTitle("DeepEnds.Console")]
+[assembly: AssemblyDescription("Architectural dependency analysis")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("Zebedee Mason")]
+[assembly: AssemblyProduct("DeepEnds")]
+[assembly: AssemblyCopyright("Copyright © 2016 LGPL 2.1")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +58,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
---
+++ checks.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+#
+# Copyright 2016 Zebedee Mason
+#
+#  The author's copyright is expressed through the following notice, thus
+#  giving effective rights to copy and use this software to anyone, as shown
+#  in the license text.
+#
+# NOTICE:
+#  This software is released under the terms of the GNU Lesser General Public
+#  license; a copy of the text has been released with this package (see file
+#  LICENSE, where the license text also appears), and can be found on the
+#  GNU web site, at the following address:
+#
+#           http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+#
+#  Please refer to the license text for any license information. This notice
+#  has to be considered part of the license, and should be kept on every copy
+#  integral or modified, of the source files. The removal of the reference to
+#  the license will be considered an infringement of the license itself.
+import sys, os, os.path, json
+
+def PrintLine(filename, lineno):
+	print(filename)
+	index = 0
+	for line in open(filename):
+		index += 1
+		if index == lineno:
+			print(line.rstrip())
+			break
+
+for root, dirs, files in os.walk("."):
+	for  name in files:
+		if name != "AssemblyInfo.cs":
+			continue
+		filename = os.path.join(root, name)
+		print(filename)
+		for line in open(filename):
+			if -1 == line.find("Version"):
+				continue
+			if line[:1] != '[':
+				continue
+			print(line.rstrip())
+		
+PrintLine("GUI\\RelNotes.txt", 1)
+PrintLine("GUI\\License.txt", 1)
+PrintLine("GUI\\source.extension.vsixmanifest", 4)
